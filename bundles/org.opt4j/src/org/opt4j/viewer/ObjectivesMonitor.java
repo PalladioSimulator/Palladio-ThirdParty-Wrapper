bf3a9a3685598dd61b848e02d1bbbfbc69003f71
Nicolas-Boltz <boltz.nicolas@gmx.de>
2026-08-10 09:45:10 +0200

    Migrate javax.inject/javax.annotation to jakarta
    Eclipse 2023-12 ships the jakarta.* variants of the annotation APIs and
    Xtext 2.33 itself uses jakarta.inject. JDK packages such as javax.measure,
    javax.swing and javax.annotation.processing are deliberately untouched.

--- bundles/org.opt4j/src/org/opt4j/viewer/ObjectivesMonitor.java
+++ bundles/org.opt4j/src/org/opt4j/viewer/ObjectivesMonitor.java
@@ -21,8 +21,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import org.opt4j.core.Individual;
 import org.opt4j.core.Individual.State;
